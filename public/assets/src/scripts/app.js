9075ccaaf6c501c4049bc01925fc22657a7e2acf
Justin Bull <bull.justin@gmail.com>
2016-10-22 18:35:58 -0700

    Move hero section into a html views partial

--- public/assets/src/scripts/app.js
+++ public/assets/src/scripts/app.js
@@ -1,4 +1,19 @@
-console.log("99 little bugs in the code");
-console.log("99 bugs in the code");
-console.log("patch one down, compile it around");
-console.log("117 bugs in the code");
+console.log('99 little bugs in the code');
+console.log('99 bugs in the code');
+console.log('patch one down, compile it around');
+console.log('117 bugs in the code');
+
+(()=> {
+
+  'use strict';
+
+  const importNode = (name) => {
+    let container = document.getElementById('CssAssessment');
+    let link = document.querySelector(`link[href="views/${name}.html"]`);
+    let content = document.importNode(link.import.body.firstChild, true);
+    container.appendChild(content);
+  };
+
+  importNode('hero');
+
+})();
